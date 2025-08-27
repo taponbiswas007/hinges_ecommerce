4268675fd3ed93efddfae76110005829ae95db9a
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-08-28 00:45:20 +0600

    first updated

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // Show the button when scrolled down 100px
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.backToTopBtn').fadeIn();
+        } else {
+            $('.backToTopBtn').fadeOut();
+        }
+    });
+
+    // Scroll to top when button is clicked
+    // $('.backToTopBtn').click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: 0
+    //     }, 500, 'linear'); // smooth and same speed
+    //     return false;
+    // });
+
+
+});
+document.querySelector('.backToTopBtn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // same-speed smooth scroll
+    });
+});
+
+$(document).ready(function () {
+    // Hide preloader after 2 seconds
+    setTimeout(function () {
+        $('.preloader').fadeOut('slow');
+    }, 2000);
+});
