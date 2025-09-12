88008733c50dc784bc6ada24ed39035b8a254f69
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-09-13 00:03:42 +0600

    fhfg

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,21 @@ function updateNavButtons(swiper) {
 // shop by category slider end
 
 
+// product slider start
+  var swiper = new Swiper(".productDetailsSlider", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".productDetailsSlider2", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+
